b8eac13ebc95aef564b3178e6f6acbce456af6c4
york <york@york.(none)>
2014-02-14 17:12:20 +0800

    再次修改导航图的清晰度

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,16 +25,18 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fit => [800, 800]
+  #process :resize_to_fit => [800, 800]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
+	# 
 	version :bananer do 
-		process :resize_to_fill => [794,357]
+		process :resize_to_fit => [794,357]
 	end
+
    version :thumb do
      process :resize_to_fit => [380, 380]
    end
