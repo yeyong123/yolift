09027ee065539782a50bf9491ecc01f80da49eaf
yeyong14 <yeyong14@gmail.com>
2014-03-12 13:56:14 +0800

    加了回到顶部jquery代码

--- app/assets/javascripts/js/customer.js
+++ app/assets/javascripts/js/customer.js
@@ -232,4 +232,32 @@ $('.getnumber').hover(
 $('#table').filterTable();
 
 //产品展示页面的固定定位
+//
+//回到顶部的代码
+//
+function goTop()
+{
+	$(window).scroll(function(e) {
+		//若滚动条离顶部大于100元素
+		if($(window).scrollTop()>300)
+		$("#gotop").fadeIn(1000);//以1秒的间隔渐显id=gotop的元素
+		else
+		$("#gotop").fadeOut(1000);//以1秒的间隔渐隐id=gotop的元素
+	});
+};
+$(function(){
+	//点击回到顶部的元素
+	$("#gotop").click(function(e) {
+		//以1秒的间隔返回顶部
+		$('body,html').animate({scrollTop:0},1000);
+	});
+	$("#gotop").mouseover(function(e) {
+		$(this).css("background","url(/assets/top.png) no-repeat 0px 0px");
+	});
+	$("#gotop").mouseout(function(e) {
+		$(this).css("background","url(/assets/top.png) no-repeat -70px 0px");
+	});
+	goTop();//实现回到顶部元素的渐显与渐隐
+});
+
 });
---
+++ config/initializers/kaminari.rb
@@ -0,0 +1,4 @@
+#Kaminari.configure do |config|
+#	  config.page_method_name = :per_page_kaminari
+#		config.page_method_name = :per_page_wice_grid
+#end
