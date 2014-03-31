64229b84a78a930d06d794ed99e1ac24277a8424
yeyong14 <yeyong14@gmail.com>
2014-03-31 17:36:07 +0800

    footer color fixed

--- app/assets/javascripts/js/customer.js
+++ app/assets/javascripts/js/customer.js
@@ -242,7 +242,7 @@ function goTop()
 {
 	$(window).scroll(function(e) {
 		//若滚动条离顶部大于100元素
-		if($(window).scrollTop()>300)
+		if($(window).scrollTop()>500)
 		$("#gotop").fadeIn(1000);//以1秒的间隔渐显id=gotop的元素
 		else
 		$("#gotop").fadeOut(1000);//以1秒的间隔渐隐id=gotop的元素
@@ -250,15 +250,9 @@ function goTop()
 };
 $(function(){
 	//点击回到顶部的元素
-	$("#gotop").click(function(e) {
+	$("#top").click(function(e) {
 		//以1秒的间隔返回顶部
 		$('body,html').animate({scrollTop:0},1000);
-	});
-	$("#gotop").mouseover(function(e) {
-		$(this).css("background","url(/assets/top.png) no-repeat 0px 0px");
-	});
-	$("#gotop").mouseout(function(e) {
-		$(this).css("background","url(/assets/top.png) no-repeat -70px 0px");
 	});
 	goTop();//实现回到顶部元素的渐显与渐隐
 });
