fdeccdb5b779329c89b6124571abe0a496c976cf
yeyong14 <yeyong14@gmail.com>
2014-05-23 22:09:10 +0800

    单个产品页面

--- app/assets/javascripts/mobile/mobile_yolift.js
+++ app/assets/javascripts/mobile/mobile_yolift.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 	$('.flexslider').flexslider({
 		animation: "slide",
+		directionNav: false
+	});
+	$('.flexslider-show').flexslider({
+		animation: "slide",
 		directionNav: false,
+		controlNav: "thumbnails"
 	});
 });
