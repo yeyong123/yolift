85f01877c6c801e70012bb214de7a0b08553d153
yeyong14 <yeyong14@gmail.com>
2014-07-28 12:25:25 +0800

    修改了百度的链接

---
+++ app/assets/javascripts/baidu.js
@@ -0,0 +1,3 @@
+	var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+			  document.write(unescape("%3Cscript async src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F258ce2c169d214905ef32e5c177564af' type='text/javascript'%3E%3C/script%3E"));
+
